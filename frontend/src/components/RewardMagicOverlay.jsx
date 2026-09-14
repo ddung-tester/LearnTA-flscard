6ecb41a158229a522895894608a1bc3aab053b1b
Dung Dinh <ddung.tester@gmail.com>
2026-09-14 17:35:04 +0700

    feat: add RewardMagicOverlay animation component and AGENTS documentation

--- frontend/src/components/RewardMagicOverlay.jsx
+++ frontend/src/components/RewardMagicOverlay.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import "./RewardMagicOverlay.css";
